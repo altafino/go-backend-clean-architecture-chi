e1979ba29cb876d2df9ae16112ccfe7b97976dd1
Stefan Wuthrich <agilo.ch@gmail.com>
2024-09-18 00:33:43 -0300

    Add CORS

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 
 	"github.com/altafino/go-backend-clean-architecture-chi/api/route"
 	"github.com/altafino/go-backend-clean-architecture-chi/bootstrap"
@@ -22,6 +23,17 @@ func main() {
 	timeout := time.Duration(env.ContextTimeout) * time.Second
 
 	r := chi.NewRouter()
+
+	// CORS middleware
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"}, // Use your allowed origins
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "Origin", "X-Requested-With"},
+		ExposedHeaders:   []string{"Link", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	r.Use(cors.Handler)
 
 	route.Setup(env, timeout, db, r)
 
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
